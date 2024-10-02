4e88262497ce900c70cab5fc64ecee77ad403ef7
vohoangloc123 <you@example.com>
2024-10-02 12:33:39 +0700

    Add 'riverpod' & A Provider

---
+++ lib/providers/user_places.dart
@@ -0,0 +1,10 @@
+import 'package:favorite_places_app/models/place.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserPlacesNotifier extends StateNotifier<List<Place>> {
+  UserPlacesNotifier() : super(const []);
+  void addPlace(String title) {
+    final newPlace = Place(title: title);
+  }
+}
