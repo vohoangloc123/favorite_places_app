37990993c469450421d2fc892a1f937e7ee7ffd7
vohoangloc123 <you@example.com>
2024-10-04 13:32:31 +0700

    Use the Device Camera For Taking Pictures

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({super.key});
@@ -8,9 +11,37 @@ class ImageInput extends StatefulWidget {
 }
 
 class _ImageInputState extends State<ImageInput> {
-  void _takePicture() {}
+  File? _selectedImage;
+  void _takePicture() async {
+    final imagePicker = ImagePicker();
+    final pickedImage =
+        await imagePicker.pickImage(source: ImageSource.camera, maxWidth: 600);
+    if (pickedImage == null) {
+      return;
+    }
+    setState(() {
+      _selectedImage = File(pickedImage.path);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = TextButton.icon(
+      icon: const Icon(Icons.camera),
+      onPressed: _takePicture,
+      label: const Text('Take Picture'),
+    );
+    if (_selectedImage != null) {
+      content = GestureDetector(
+        onTap: _takePicture,
+        child: Image.file(
+          _selectedImage!,
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+      );
+    }
     return Container(
       decoration: BoxDecoration(
         border: Border.all(
@@ -20,11 +51,7 @@ class _ImageInputState extends State<ImageInput> {
       height: 250,
       width: double.infinity,
       alignment: Alignment.center,
-      child: TextButton.icon(
-        icon: const Icon(Icons.camera),
-        onPressed: _takePicture,
-        label: const Text('Take Picture'),
-      ),
+      child: content,
     );
   }
 }
